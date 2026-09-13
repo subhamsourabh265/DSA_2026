48bd4bdd9fbca311491339bc4bd64f50b24becab
Subham Sourabh <sourabh.shubham120@gmail.com>
2026-09-13 21:41:22 +0530

    basic: search elements in an array

---
+++ Basic/searchArr.js
@@ -0,0 +1,10 @@
+searchElement = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(searchElement([1, 2, 3, 4, 5], 3)); // returns 2
